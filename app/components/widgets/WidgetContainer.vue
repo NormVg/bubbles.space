9cb717be4f54c2bfb76da98ba49038457b1763aa
Vishnu_Mac <thenormvg@gmail.com>
2026-07-02 14:48:38 +0530

    fix: drag handle overlapping widget content

--- app/components/widgets/WidgetContainer.vue
+++ app/components/widgets/WidgetContainer.vue
@@ -332,11 +332,10 @@ html.light .widget:hover {
 
 /* Drag Handle & Title */
 .widget-drag-handle {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
+  position: relative;
+  width: 100%;
   height: 36px;
+  flex-shrink: 0;
   display: flex;
   flex-direction: column;
   align-items: center;
