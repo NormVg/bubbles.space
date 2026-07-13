ff2449d31e7cd6fce3f8a9ff28a1832e339ae64f
Vishnu_Mac <thenormvg@gmail.com>
2026-07-13 08:05:25 +0530

    fix: serve sitemap.xml via nitro route to ensure correct content type

--- server/routes/sitemap.xml.ts
+++ server/routes/sitemap.xml.ts
@@ -1,4 +1,7 @@
-<?xml version="1.0" encoding="UTF-8"?>
+export default defineEventHandler((event) => {
+  setResponseHeader(event, 'Content-Type', 'text/xml');
+  
+  return `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://bubbles.taohq.org/</loc>
@@ -20,4 +23,5 @@
     <changefreq>monthly</changefreq>
     <priority>0.7</priority>
   </url>
-</urlset>
+</urlset>`;
+});
